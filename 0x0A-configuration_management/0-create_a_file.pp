dc6f6f4992fe6acab4b3dc37469d1b45f482da9d
Sokayna <sokayna.bennasseur@gmail.com>
2023-09-27 06:49:42 -0400

    remove: directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# CAreate a file using Puppet
-file { 'school':
-  path    => '/tmp/school',
-  force   => 'true',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Execute a command
-exec { 'pkill killmenow':
-  path    => '/usr/bin/',
-  command => 'pkill killmenow',
-}
