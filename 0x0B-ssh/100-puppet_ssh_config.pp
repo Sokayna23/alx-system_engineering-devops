849e3f833c61eb8429b053bc410b4f3ce7b7cbc4
Sokayna <sokayna.bennasseur@gmail.com>
2024-01-27 06:17:30 -0500

    task Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# SSH configuration file so that you can connect to a server without typing a password.
+file_line { 'Turn off passwr auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path   =>  '/etc/ssh/ssh_config',
+  line   =>  'IdentityFile ~/.ssh/school',
+  ensure => present,
+}
